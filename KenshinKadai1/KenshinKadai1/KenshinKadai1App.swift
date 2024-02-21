4e3dffa0aaaf35c70e7f37616052aad711cb9a0f
Kenshin888 <160022857+Kenshin888@users.noreply.github.com>
2024-02-21 23:27:54 +0900

    Add files via upload

---
+++ KenshinKadai1/KenshinKadai1/KenshinKadai1App.swift
@@ -0,0 +1,17 @@
+//
+//  KenshinKadai1App.swift
+//  KenshinKadai1
+//
+//  Created by Kenshin on 2024/02/21.
+//
+
+import SwiftUI
+
+@main
+struct KenshinKadai1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
